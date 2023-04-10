2b9ed26d3e44db638f2e8319d00c813a090337e2
Thorge007 <130246214+Thorge007@users.noreply.github.com>
2023-04-10 15:26:55 -0400

    Add files via upload

---
+++ T9 Coding Challenge 2/countdown9-2.js
@@ -0,0 +1,51 @@
+"use strict"
+
+/*
+   New Perspectives on HTML5 and CSS3, 8th Edition
+   Tutorial 9
+   Coding Challenge 2
+
+   Countdown Clock
+   Author: Thomas Hendren
+   Date:   12/4/2020
+
+
+*/
+
+var secsLeft = 10;
+var clockID = setInterval(countdown, 1000);
+
+function countdown() {
+
+  var secsString = addLeadingZero(secsLeft);
+  document.getElementById("seconds").textContent = secsString;
+  checkCountdown();
+  secsLeft--;
+}
+
+function stopCountdown() {
+  document.getElementById("Alert").textContent - "Time's Up"
+  clearInterval(clockID);
+}
+
+
+
+
+/* ------------------------------------------------- */
+
+/* The checkCountdown() function tests whether there is any time left to make the
+   ticket order. If the time left is 0, the stopClock() function is run;
+   otherwise nothing happens and the program continues to run. */
+   
+function checkCountdown() {
+   if (secsLeft === 0) stopCountdown();
+}
+
+
+/* The addLeadingZero() function adds a leading zero to values which are less than 10 */
+
+function addLeadingZero(num) {
+   var numStr = (num < 10) ? ("0" + num) : "" + num;
+   return numStr;
+}
+
